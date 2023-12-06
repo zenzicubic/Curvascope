5dde21c9550bc31a4b28465e728c73a6f7332e3d
Zenzicubic <58337545+zenzicubic@users.noreply.github.com>
2023-12-06 15:20:43 -0500

    Fix a selector bug

--- src/menus/SettingsMenu/colorpicker/ColorPicker.js
+++ src/menus/SettingsMenu/colorpicker/ColorPicker.js
@@ -8,7 +8,7 @@
 import './ColorPicker.css';
 
 import React from 'react';
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 
 // Colors to display
 const colors = [[244, 67, 54], [233, 30, 99], [156, 39, 176], [103, 58, 183], [63, 81, 181], [33, 150, 243], [3, 169, 244], [0, 188, 212], [0, 150, 136], [76, 175, 80], [139, 195, 74], [205, 220, 57], [255, 235, 59], [255, 235, 59], [255, 193, 7], [255, 152, 0], [255, 87, 34], [183, 28, 28], [136, 14, 79], [74, 20, 140], [49, 27, 146], [13, 71, 161], [1, 87, 155], [0, 96, 100], [0, 77, 64], [27, 94, 32], [51, 105, 30], [130, 119, 23], [245, 127, 23], [255, 111, 0], [230, 81, 0], [191, 54, 12], [250, 250, 250], [96, 125, 139]];
@@ -23,23 +23,29 @@ Color picker component.
 function ColorPicker(props) {
     const [colIdx, setColIdx] = useState(props.selIdx);
 
-    const handleKeypress = useCallback((evt) => {
-        let nIdx;
+    const handleKeypress = (evt) => {
+        let nIdx, updated = false;
 
-        // Check if any key pressed
+        // Get new index on key press
         switch (evt.key) {
             default:
                 break;
             case "ArrowLeft":
-                setColIdx(idx => (nIdx = Math.max(0, idx - 1)));
-                props.onChange(nIdx);
+                nIdx = Math.max(colIdx - 1, 0);
+                updated = true;
                 break;
             case "ArrowRight":
-                setColIdx(idx => (nIdx = Math.min(idx + 1, colors.length - 1)));
-                props.onChange(nIdx);
+                nIdx = Math.min(colIdx + 1, colors.length - 1);
+                updated = true;
                 break;
         }
-    }, [props]);
+
+        // Update values if changed
+        if (updated) {
+            setColIdx(nIdx);
+            props.onChange(nIdx);
+        }
+    };
 
     return (
     <div className="color-picker" tabIndex="0" onKeyDown={handleKeypress}>
